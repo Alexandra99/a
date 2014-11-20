2a67be413d16bcfafb858d7ace48b6e8ae59fbe1
Alex48429 <user@user-PC>
2014-11-20 13:23:39 +0200

    new commit

--- src/a/A.java
+++ src/a/A.java
@@ -17,6 +17,7 @@ public class A {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     System.out.println("a");}
+     System.out.println("a");
+     System.out.println("dokimi");}
        
 }
